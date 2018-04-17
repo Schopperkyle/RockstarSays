e7f441f3a0eef453800d471eb9b5e1883b8f689a
kyleschopper <kjs0031@auburn.edu>
2018-04-17 10:16:27 -0500

    Initial Commit

--- app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/Rockstars/MainActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/Rockstars/MainActivity.java
@@ -2,6 +2,17 @@ package edu.auburn.eng.csse.comp3710.spring2018.Rockstars;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.os.CountDownTimer;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/Rockstars/MainMenu.java
@@ -0,0 +1,17 @@
+package edu.auburn.eng.csse.comp3710.spring2018.Rockstars;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
